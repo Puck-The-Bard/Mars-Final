a72b0a118aecad368a776d822fa976df999ba136
Puck-The-Bard <zdmcguire1@buffs.wtamu.edu>
2019-12-09 09:36:08 -0600

    change port

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('*', function(req,res) {
     res.sendfile(Path.join(__dirname + '/dist/MarsFinal/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
