bef3072b260dbefe2842169179ccf52a6e0c42fd
Puck-The-Bard <zdmcguire1@buffs.wtamu.edu>
2019-12-09 04:41:59 -0600

    setting up heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory 
+
+app.use(express.static(__dirname + '/dist/Marsfinal'));
+
+app.get('*', function(req,res) {
+
+    res.sendfile(Path.join(__dirname + '/dist/MarsFinal/app.component.html'));
+});
+
+app.listen(process.env.PORT || 8080);
